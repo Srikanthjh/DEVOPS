5d400b030d1f5c6ba1708914756fb5065bfcef72
EC2 Default User <ec2-user@ip-172-31-17-218.ap-southeast-1.compute.internal>
2022-04-16 11:47:34 +0000

    first commit c1

---
+++ big3.c
@@ -0,0 +1,176 @@
+class Example {
+public:
+  Example(int x_in, double y_in)
+    : x(x_in), y(y_in) {}
+
+  Example(const Example &other)
+    : x(other.x), y(other.y) {
+    count << "Example copy ctor: " << x << ", " << y << endl;
+  }
+
+  int get_x() const {
+    return x;
+  }
+
+  double get_y() const {
+    return y;
+  }
+
+private:
+  int x;
+  double y;
+};
+class Example {
+public:
+  Example(int x_in, double y_in)
+    : x(x_in), y(y_in) {}
+
+  Example(const Example &other)
+    : x(other.x), y(other.y) {
+    count << "Example copy ctor: " << x << ", " << y << endl;
+  }
+
+  int get_x() const {
+    return x;
+  }
+
+  double get_y() const {
+    return y;
+  }
+
+private:
+  int x;
+  double y;
+};
+class Example {
+public:
+  Example(int x_in, double y_in)
+    : x(x_in), y(y_in) {}
+
+  Example(const Example &other)
+    : x(other.x), y(other.y) {
+    count << "Example copy ctor: " << x << ", " << y << endl;
+  }
+
+  int get_x() const {
+    return x;
+  }
+
+  double get_y() const {
+    return y;
+  }
+
+private:
+  int x;
+  double y;
+};
+class Example {
+public:
+  Example(int x_in, double y_in)
+    : x(x_in), y(y_in) {}
+
+  Example(const Example &other)
+    : x(other.x), y(other.y) {
+    count << "Example copy ctor: " << x << ", " << y << endl;
+  }
+
+  int get_x() const {
+    return x;
+  }
+
+  double get_y() const {
+    return y;
+  }
+
+private:
+  int x;
+  double y;
+};
+class Example {
+public:
+  Example(int x_in, double y_in)
+    : x(x_in), y(y_in) {}
+
+  Example(const Example &other)
+    : x(other.x), y(other.y) {
+    count << "Example copy ctor: " << x << ", " << y << endl;
+  }
+
+  int get_x() const {
+    return x;
+  }
+
+  double get_y() const {
+    return y;
+  }
+
+private:
+  int x;
+  double y;
+};
+class Example {
+public:
+  Example(int x_in, double y_in)
+    : x(x_in), y(y_in) {}
+
+  Example(const Example &other)
+    : x(other.x), y(other.y) {
+    count << "Example copy ctor: " << x << ", " << y << endl;
+  }
+
+  int get_x() const {
+    return x;
+  }
+
+  double get_y() const {
+    return y;
+  }
+
+private:
+  int x;
+  double y;
+};
+class Example {
+public:
+  Example(int x_in, double y_in)
+    : x(x_in), y(y_in) {}
+
+  Example(const Example &other)
+    : x(other.x), y(other.y) {
+    count << "Example copy ctor: " << x << ", " << y << endl;
+  }
+
+  int get_x() const {
+    return x;
+  }
+
+  double get_y() const {
+    return y;
+  }
+
+private:
+  int x;
+  double y;
+};
+class Example {
+public:
+  Example(int x_in, double y_in)
+    : x(x_in), y(y_in) {}
+
+  Example(const Example &other)
+    : x(other.x), y(other.y) {
+    count << "Example copy ctor: " << x << ", " << y << endl;
+  }
+
+  int get_x() const {
+    return x;
+  }
+
+  double get_y() const {
+    return y;
+  }
+
+private:
+  int x;
+  double y;
+};
